aa372216fc7dbb79573943b23369daceda42f0a5
0716302 <chick890417@gmail.com>
2022-05-14 15:25:15 +0800

    first con

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -1,13 +1,15 @@
 import { Potter } from './potter';
 
 describe('Potter', () => {
+  let potter: Potter;
+  beforeEach(() => {
+    potter = new Potter();
+  });
   it('buy 0 book', () => {
-    const potter = new Potter();
     const pay=potter.price(0);
     expect(pay).toBe(0);
   });
   it('buy 1 book', () => {
-    const potter = new Potter();
     const pay=potter.price(1);
     expect(pay).toBe(8);
   });
