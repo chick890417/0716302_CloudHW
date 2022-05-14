c7ee1eca6569da533df592276c0b96133737603c
0716302 <chick890417@gmail.com>
2022-05-14 20:49:59 +0800

    second con

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -5,18 +5,30 @@ describe('Potter', () => {
   beforeEach(() => {
     potter = new Potter();
   });
+  it('should create an instance', () => {
+    expect(potter).toBeTruthy();
+  });
   it('buy 0 book', () => {
-    const pay=potter.price([]);
-    expect(pay).toBe(0);
+    expect(potter.price([])).toBe(0);
   });
   it('buy 1 book', () => {
     let books=[0];
-    const pay=potter.price(books);
-    expect(pay).toBe(8);
+    expect(potter.price(books)).toBe(8);
   });
   it('buy 2 diff book', () => {
     let books=[0,1];
-    const pay=potter.price(books);
-    expect(pay).toBe(8*2*0.95);
+    expect(potter.price(books)).toBe(8*2*0.95);
+  });
+  it('buy 3 diff book', () => {
+    let books=[0,1,2];
+    expect(potter.price(books)).toBe(8*3*0.9);
+  });
+  it('buy 4 diff book', () => {
+    let books=[0,1,2,3];
+    expect(potter.price(books)).toBe(8*4*0.8);
+  });
+  it('buy 5 diff book', () => {
+    let books=[0,1,2,3,4];
+    expect(potter.price(books)).toBe(8*5*0.75);
   });
 });
